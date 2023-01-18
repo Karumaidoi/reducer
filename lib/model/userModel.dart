136b0a6764b52ca95dc72a4e30663e0856236ff6
Karumaidoi <mainahmwangi12@gmail.com>
2023-01-18 13:53:54 +0300

    Comments on User Model

--- lib/model/userModel.dart
+++ lib/model/userModel.dart
@@ -1,15 +1,19 @@
+// A class responsible for formatting our User Object
 class UserModel {
+  // Initializing our Required fields
   late String name;
   late String email;
   late String occupation;
   late String bio;
 
+  // Making our Class constructor
   UserModel(
       {required this.name,
       required this.email,
       required this.occupation,
       required this.bio});
 
+  // Factorizing our properties with data from our Object
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(
         email: json['email'],
